fcee434d9cce3f5bfe302fd2469d2c6472a66e24
nya3_neko2 <takahashinaoki521@gmail.com>
2023-06-26 03:11:27 +0900

    Initial Commit

---
+++ OonishiLayoutKeyboard/OonishiLayoutKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OonishiLayoutKeyboardApp.swift
+//  OonishiLayoutKeyboard
+//
+//  Created by 高橋直希 on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct OonishiLayoutKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
